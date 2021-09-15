e7744c9f920d365a0d80c837b5abafcb4171651d
Moody <76251897+moodyhunter@users.noreply.github.com>
2021-09-15 19:31:13 +0800

    New translations PD_en_US.ts (Chinese Simplified)

---
+++ translations/PD_zh_CN.ts
@@ -0,0 +1,117 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh-CN" sourcelanguage="en">
+  <context>
+    <name>LoginPage</name>
+    <message>
+      <source>Enter Your Password</source>
+      <translation type="unfinished">Enter Your Password</translation>
+    </message>
+    <message>
+      <source>Unlock Database</source>
+      <translation type="unfinished">Unlock Database</translation>
+    </message>
+    <message>
+      <source>Failed to Open Database</source>
+      <translation type="unfinished">Failed to Open Database</translation>
+    </message>
+  </context>
+  <context>
+    <name>MainComponent</name>
+    <message>
+      <source>Dashboard</source>
+      <translation type="unfinished">Dashboard</translation>
+    </message>
+    <message>
+      <source>All activity</source>
+      <translation type="unfinished">All activity</translation>
+    </message>
+  </context>
+  <context>
+    <name>PD::PDMainWindow</name>
+    <message>
+      <source>PD - The Personal Dashboard</source>
+      <translation type="unfinished">PD - The Personal Dashboard</translation>
+    </message>
+  </context>
+  <context>
+    <name>PanelGrid</name>
+    <message>
+      <source>Delete</source>
+      <translation type="unfinished">Delete</translation>
+    </message>
+    <message>
+      <source>Edit</source>
+      <translation type="unfinished">Edit</translation>
+    </message>
+    <message>
+      <source>Add</source>
+      <translation type="unfinished">Add</translation>
+    </message>
+  </context>
+  <context>
+    <name>QObject</name>
+    <message>
+      <source>QSQLiteResult</source>
+      <comment>Unable to fetch row</comment>
+      <translation type="unfinished">QSQLiteResult</translation>
+    </message>
+    <message>
+      <source>QSQLiteResult</source>
+      <comment>No query</comment>
+      <translation type="unfinished">QSQLiteResult</translation>
+    </message>
+    <message>
+      <source>QSQLiteResult</source>
+      <comment>Unable to execute statement</comment>
+      <translation type="unfinished">QSQLiteResult</translation>
+    </message>
+    <message>
+      <source>QSQLiteResult</source>
+      <comment>Unable to execute multiple statements at a time</comment>
+      <translation type="unfinished">QSQLiteResult</translation>
+    </message>
+    <message>
+      <source>QSQLiteResult</source>
+      <comment>Unable to reset statement</comment>
+      <translation type="unfinished">QSQLiteResult</translation>
+    </message>
+    <message>
+      <source>QSQLiteResult</source>
+      <comment>Unable to bind parameters</comment>
+      <translation type="unfinished">QSQLiteResult</translation>
+    </message>
+    <message>
+      <source>QSQLiteResult</source>
+      <comment>Parameter count mismatch</comment>
+      <translation type="unfinished">QSQLiteResult</translation>
+    </message>
+  </context>
+  <context>
+    <name>QSQLCipherDriver</name>
+    <message>
+      <source>Incorrect Password</source>
+      <translation type="unfinished">Incorrect Password</translation>
+    </message>
+    <message>
+      <source>Error opening database</source>
+      <translation type="unfinished">Error opening database</translation>
+    </message>
+    <message>
+      <source>Error closing database</source>
+      <translation type="unfinished">Error closing database</translation>
+    </message>
+    <message>
+      <source>Unable to begin transaction</source>
+      <translation type="unfinished">Unable to begin transaction</translation>
+    </message>
+    <message>
+      <source>Unable to commit transaction</source>
+      <translation type="unfinished">Unable to commit transaction</translation>
+    </message>
+    <message>
+      <source>Unable to rollback transaction</source>
+      <translation type="unfinished">Unable to rollback transaction</translation>
+    </message>
+  </context>
+</TS>
